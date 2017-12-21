4cafe11a75deac6beac4d6fe4e81f35f5950d63e
BeckyWang <wangbeibei01018@163.com>
2017-12-21 17:07:52 +0800

    add data

--- src/component/EnglishTable2/index.js
+++ src/component/EnglishTable2/index.js
@@ -2,75 +2,30 @@ import React from 'react';
 
 import styles from './styles';
 
-class EnglishTable2 extends React.Component {
-	constructor() {
-        super();
-    }
+const EnglishTable2 = ({question_type_topic}) => {
+    const tableContainer = question_type_topic.map(({desc, topic_name, topic_list}) =><li className={styles['body-item']}>
+        <span style={{width: '16%'}}>{desc}</span>
+        <span style={{width: '17%'}}>{topic_name}</span>
+        <ul className={styles['knowledge-point']} style={{width: '67%'}}>
+            { topic_list.map(({avg_score, knowledge, my_socre, total_score}) => <li>
+                <span style={{width: '31%'}}>{knowledge}</span>
+                <span style={{width: '33.5%'}}>{my_socre}</span>
+                <span style={{width: '34.5%'}}>{avg_score}/{total_score}</span>
+            </li>) }
+        </ul>
+    </li>);
 
-    render() {
-        return (<div className={styles['table-container']}>
-        	<ul className={styles['head']}>
-        		<li style={{width: '16%'}}><span>题型</span></li>
-        		<li style={{width: '36.5%'}}><span>知识点</span></li>
-        		<li style={{width: '18%'}}><span>我的得分</span></li>
-        		<li style={{width: '23.5%'}}><span>平均分/总分</span></li>
-        	</ul>
-        	<ul className={styles['body']}>
-        		<li className={styles['body-item']}>
-        			<span style={{width: '16%'}}>单选1</span>
-                    <span style={{width: '17%'}}>话题</span>
-                    <ul className={styles['knowledge-point']} style={{width: '67%'}}>
-                        <li>
-                            <span style={{width: '31%'}}>考察点</span>
-                            <span style={{width: '33.5%'}}>2</span>
-                            <span style={{width: '34.5%'}}>1.5/2</span>
-                        </li>
-                        <li>
-                            <span style={{width: '31%'}}>考察点</span>
-                            <span style={{width: '33.5%'}}>2</span>
-                            <span style={{width: '34.5%'}}>1.5/2</span>
-                        </li>
-                        <li>
-                            <span style={{width: '31%'}}>考察点</span>
-                            <span style={{width: '33.5%'}}>2</span>
-                            <span style={{width: '34.5%'}}>1.5/2</span>
-                        </li>
-                        <li>
-                            <span style={{width: '31%'}}>考察点</span>
-                            <span style={{width: '33.5%'}}>2</span>
-                            <span style={{width: '34.5%'}}>1.5/2</span>
-                        </li>
-                    </ul>
-        		</li>
-                <li className={styles['body-item']}>
-                    <span style={{width: '16%'}}>单选1</span>
-                    <span style={{width: '17%'}}>话题</span>
-                    <ul className={styles['knowledge-point']} style={{width: '67%'}}>
-                        <li>
-                            <span style={{width: '31%'}}>考察点</span>
-                            <span style={{width: '33.5%'}}>2</span>
-                            <span style={{width: '34.5%'}}>1.5/2</span>
-                        </li>
-                        <li>
-                            <span style={{width: '31%'}}>考察点</span>
-                            <span style={{width: '33.5%'}}>2</span>
-                            <span style={{width: '34.5%'}}>1.5/2</span>
-                        </li>
-                        <li>
-                            <span style={{width: '31%'}}>考察点</span>
-                            <span style={{width: '33.5%'}}>2</span>
-                            <span style={{width: '34.5%'}}>1.5/2</span>
-                        </li>
-                        <li>
-                            <span style={{width: '31%'}}>考察点</span>
-                            <span style={{width: '33.5%'}}>2</span>
-                            <span style={{width: '34.5%'}}>1.5/2</span>
-                        </li>
-                    </ul>
-                </li>
-        	</ul>
-        </div>)
-    }
-}
+    return <div className={styles['table-container']}>
+        <ul className={styles['head']}>
+            <li style={{width: '16%'}}><span>题型</span></li>
+            <li style={{width: '36.5%'}}><span>知识点</span></li>
+            <li style={{width: '18%'}}><span>我的得分</span></li>
+            <li style={{width: '23.5%'}}><span>平均分/总分</span></li>
+        </ul>
+        <ul className={styles['body']}>
+            { tableContainer }
+        </ul>
+    </div>; 
+};
 
 export default EnglishTable2;
--- src/component/QRcode/index.js
+++ src/component/QRcode/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './styles';
 import qrcode_bg from '../../assets/images/qrcode.png';
 
-const QRcode = ({qrcode_url}) => <div className={styles['code-container']}>
+const QRcode = ({qrcode_url, foot_notice}) => <div className={styles['code-container']}>
     <p className={styles['title']}>二维码</p>
-    <p className={styles['description']}>与iPhone X一同发布，但上市时间早近6个周的iPhone 8和iPhone 8 Plus，在iPhone中的份额还是要低于上市时间更晚的iPhone X。Localytics的数据就表明，截止12月18日，iPhone 8在iPhone中的份额为3%，iPhone 8 Plus略高，为3.69%。</p>           
+    <p className={styles['description']}>{foot_notice}</p>           
     <div className={styles['qr-code']}>
         <img src={qrcode_bg} className={styles['transparent-bg']}/>
         <img src={qrcode_bg} className={styles['non-transparent-bg']}/>

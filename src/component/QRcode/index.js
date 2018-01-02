db304b553793b2d75f31edadbc9cd1a90eee8174
BeckyWang <wangbeibei01018@163.com>
2018-01-02 14:31:11 +0800

    modify QRcode styles

--- src/component/QRcode/index.js
+++ src/component/QRcode/index.js
@@ -3,7 +3,7 @@ import styles from './styles';
 import qrcode_bg from '../../assets/images/qrcode.png';
 
 const QRcode = ({qrcode_url, foot_notice, foot_extra}) => <div className={styles['code-container']}>
-    <p className={styles['title']}>二维码</p>
+    <p className={styles['title']}>老师寄语</p>
     <p className={styles['description']}>{foot_notice}</p>
     {foot_extra.map(value => <p className={styles['description']}>{value}</p>)}
     <div className={styles['qr-code']}>
